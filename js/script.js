1e91a75a567c54e310cb135b436111c9a0ba84b5
Stizz <stizz@Stizzys-Mac.local>
2020-02-20 13:40:17 -0500

    search button visibility input

--- js/script.js
+++ js/script.js
@@ -5,33 +5,47 @@ var apiKeyOMDB = "a648d87c"
 
 // search button captures the input you type in
 
-document.getElementById('searchButtonForm').addEventListener("submit", function(event) {
+document.getElementById('searchButtonForm').addEventListener("submit", function (event) {
   event.preventDefault();
   var searchValue = document.getElementById("searchButtonInput").value;
   var omdbQueryURL = "http://www.omdbapi.com/?t=" + searchValue + "&apikey=" + apiKeyOMDB;
   var searchButton
   var searchButtonInput
   var searchButtonForm
+  
+  $(document).on("click", "#searchButton", function() {
+    
+    $(".invisible").removeClass("invisible").addClass("visible");
+     console.log("working");
 
-  console.log(searchValue)
-
-  // takes that string and sends it as an API call to OMDB
-
-  $.ajax({
-    url: omdbQueryURL,
-    method: "GET"
-  }).then(function(response) {
-    // OMDB returns a JSON form
-    console.log(response);
-    console.log(JSON.stringify(response));
-    console.log(response.Year);
-    console.log(response.Title);
-    console.log(response.Soundtrack);
-    console.log(response.Song);
-    console.log(response.Actors).join("\n");
+      // if(searchInput == ""){
+      //   removeClass("visible")
+         
+      // }else {
+      //     removeClass("invisible")
+      // }
   });
   
-});
+  console.log(searchValue)
+  
+
+        // takes that string and sends it as an API call to OMDB
+
+        $.ajax({
+          url: omdbQueryURL,
+          method: "GET"
+        }).then(function (response) {
+          // OMDB returns a JSON form
+          console.log(response);
+          console.log(JSON.stringify(response));
+          console.log(response.Year);
+          $("#titleEl").append(response.Title);
+          console.log(response.Soundtrack);
+          console.log(response.Song);
+          console.log(response.Actors).join("\n");
+        });
+
+    });
 
 
 // Go through the JSON form and pull data from the Objects
