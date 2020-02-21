8005ece670521c30fb897db85c2dfe676dd2200c
Stizz <stizz@Stizzys-Mac.local>
2020-02-21 14:26:31 -0500

    prevent search button from refreshing the page and added visible and invisible functions to the search button

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
 var apiKeyYoutube = "AIzaSyCeb5HlrnsOhcdQDoC91dpbZe2Wo_Onibo"
 var apiKeyOMDB = "a648d87c"
-
+addClass = ""
 // working string = http://www.omdbapi.com/?t=rocky&apikey=a648d87c
 
 // search button captures the input you type in
 
+// $('#searchButtonForm').on("submit", );
 document.getElementById('searchButtonForm').addEventListener("submit", function (event) {
   event.preventDefault();
   var searchValue = document.getElementById("searchButtonInput").value;
   console.log(searchValue);
   var omdbQueryURL = "http://www.omdbapi.com/?t=" + searchValue + "&apikey=" + apiKeyOMDB;
-  var searchButton 
-  var searchButtonInput
-  var searchButtonForm
-  
-  // $(document).on("click", "#searchButton", function() {
-    
-    $(".invisible").removeClass("invisible").addClass("visible");
-     //console.log("working");
+  var searchButtonInput = $("#searchButtonInput")
+  if (searchButtonInput.val() === "") {
 
-      // if(searchButtonForm == ""){
-      //   addClass("visible")
-      //    console.log("yes")
-      // }else {
-      //     addClass("invisible")
-      //     console.log("no")
-      // }
-  //console.log(searchButtonForm)
+    return;
+  }
+
+  // $(document).on("click", "#searchButton", function() {
+  $("#welcome").empty();
+  //console.log("working")
+  $("#resultInfo").removeClass("invisible").addClass("visible")
+  //console.log("workingtoo");
+  $("#test").removeClass("invisible").addClass("visible")
+
+
+
 
   // takes that string and sends it as an API call to OMDB
 
   $.ajax({
     url: omdbQueryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     // OMDB returns a JSON form
     console.log(response);
     console.log(response.Title);
@@ -41,55 +40,54 @@ document.getElementById('searchButtonForm').addEventListener("submit", function 
     console.log(response.Song);
     console.log((response.Actors).split(', ')); // how do I create elements on a new line?
     $("#omdbActors").empty()
-let actorArray = ((response.Actors).split(', '));
-for (let i = 0; i < actorArray.length; i++) {
-  $("#omdbActors").append("<br />&nbsp;&nbsp;&nbsp;&nbsp;")
-  $("#omdbActors").append(actorArray[i]);//this is appending each individual list in the array
-}
-$("#omdbYear").text(response.Year)
-$("#titleEl").text(response.Title)
-$("#omdbTitle").text(response.Title)
-$("#omdbPoster").attr("src", response.Poster)
-// $("#omdbActors").append(response.Actors)//this was appending the array
+    let actorArray = ((response.Actors).split(', '));
+    for (let i = 0; i < actorArray.length; i++) {
+      $("#omdbActors").append("<br />&nbsp;&nbsp;&nbsp;&nbsp;")
+      $("#omdbActors").append(actorArray[i]);//this is appending each individual list in the array
+    }
+    $("#omdbYear").text(response.Year)
+    $("#titleEl").text(response.Title)
+    $("#omdbTitle").text(response.Title)
+    $("#omdbPoster").attr("src", response.Poster)
+    // $("#omdbActors").append(response.Actors)//this was appending the array
 
 
-// if (response.Error = "Something went wrong") {
-//   $("#omdbAlert").text("HELLO");
-// } else if (Error = "Something went wrong") {
-//   $("#omdbAlert").append("MY");
-// } else if (searchValue === "") {
-//   $("#omdbAlert").text("NAME");
-//   console.log("YEAAA");
-// } else {
-//   $("#omdbAlert").text("WALT");
-// }
+    // if (response.Error = "Something went wrong") {
+    //   $("#omdbAlert").text("HELLO");
+    // } else if (Error = "Something went wrong") {
+    //   $("#omdbAlert").append("MY");
+    // } else if (searchValue === "") {
+    //   $("#omdbAlert").text("NAME");
+    //   console.log("YEAAA");
+    // } else {
+    //   $("#omdbAlert").text("WALT");
+    // }
 
   });
 
 
-var newDiv = document.createElement("div");
-newDiv.textContent = omdbYearVar;
-$("#omdbYear").append(newDiv);
-  
+  var newDiv = document.createElement("div");
+  $("#omdbYear").append(newDiv);
+
   console.log(searchValue)
-  
-  
 
-        // takes that string and sends it as an API call to OMDB
 
-        // $.ajax({
-        //   url: omdbQueryURL,
-        //   method: "GET"
-        // }).then(function (response) {
-        //   // OMDB returns a JSON form
-        //   console.log(response);
-        //   console.log(JSON.stringify(response));
-        //   console.log(response.Year);
-        //   $("#titleEl").append(response.Title);
-        //   console.log(response.Soundtrack);
-        //   console.log(response.Song);
-        //   console.log(response.Actors).join("\n");
-        // });
+
+  // takes that string and sends it as an API call to OMDB
+
+  // $.ajax({
+  //   url: omdbQueryURL,
+  //   method: "GET"
+  // }).then(function (response) {
+  //   // OMDB returns a JSON form
+  //   console.log(response);
+  //   console.log(JSON.stringify(response));
+  //   console.log(response.Year);
+  //   $("#titleEl").append(response.Title);
+  //   console.log(response.Soundtrack);
+  //   console.log(response.Song);
+  //   console.log(response.Actors).join("\n");
+  // });
 })
     // });
 
