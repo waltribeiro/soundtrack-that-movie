084ccb5f330853c95668bb81b611c3ac06345fc1
WaltRibeiro <walt@fororchestra.com>
2020-02-24 12:28:03 -0500

    cleaned up js code

--- js/script.js
+++ js/script.js
@@ -74,48 +74,10 @@ document.getElementById('searchButtonForm').addEventListener("submit", function 
     $("#titleEl").text(response.Title)
     $("#omdbTitle").text(response.Title)
     $("#omdbPoster").attr("src", response.Poster)
-    // $("#omdbActors").append(response.Actors)//this was appending the array
-
-
-    // if (response.Error = "Something went wrong") {
-    //   $("#omdbAlert").text("HELLO");
-    // } else if (Error = "Something went wrong") {
-    //   $("#omdbAlert").append("MY");
-    // } else if (searchValue === "") {
-    //   $("#omdbAlert").text("NAME");
-    //   console.log("YEAAA");
-    // } else {
-    //   $("#omdbAlert").text("WALT");
-    // }
 
   });
 
-
-  var newDiv = document.createElement("div");
-  newDiv.textContent = omdbYearVar;
-  $("#omdbYear").append(newDiv);
-  console.log(searchValue)
-
-
-
-  // takes that string and sends it as an API call to OMDB
-
-  // $.ajax({
-  //   url: omdbQueryURL,
-  //   method: "GET"
-  // }).then(function (response) {
-  //   // OMDB returns a JSON form
-  //   console.log(response);
-  //   console.log(JSON.stringify(response));
-  //   console.log(response.Year);
-  //   $("#titleEl").append(response.Title);
-  //   console.log(response.Soundtrack);
-  //   console.log(response.Song);
-  //   console.log(response.Actors).join("\n");
-  // });
 })
-    // });
-
 
 // Go through the JSON form and pull data from the Objects
 
